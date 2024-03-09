1b634c2b6ca080c54dc9a44c8a03b9068ba7c06b
collins omondi <collinsomondi778@gmail.com>
2024-03-09 11:02:26 +0300

    Your commit message

--- routes/api/orderRoute.js
+++ routes/api/orderRoute.js
@@ -4,14 +4,32 @@ const router = express.Router();
 const db = require("../../db");
 const Order = require("../../models/Order");
 
-
+// Fetch existing orders in the db
 router.get("/", (req, res) => {
   Order.findAll()
-    .then((gigs) => {
-      console.log(gigs);
+    .then((orders) => {
+      console.log(orders);
       res.sendStatus(200);
     })
     .catch((err) => console.log("Error:", err));
 });
+
+// Add orders to the db
+router.get("/placeOrder", (req, res) => {
+  const data = {
+    orderType: "sand",
+    orderQuantity: 2,
+    orderStatus: "received"
+  }
+
+  let {orderType, orderQuantity, orderStatus} = data;
+  Order.create({
+    orderType,
+    orderQuantity,
+    orderStatus
+  })
+  .then((orders) => console.log("The following are orders placed", orders))
+  .catch(err => console.log("Couldn't place order:", err))
+} )
 
 module.exports = router;
